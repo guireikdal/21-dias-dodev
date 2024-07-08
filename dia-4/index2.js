a5424d834f32b02e44a3a93dacf8086ce5a10e7e
Guilherme Reikdal <guireikdal402911@gmail.com>
2024-07-08 19:37:58 -0300

    (Repassando att das passadas e exercícios do dia-5)

--- dia-4/index2.js
+++ dia-4/index2.js
@@ -1,18 +1,18 @@
 let nome = prompt("Olá, qual o seu nome?")
-let idade = Number(prompt("Qual a sua idade"))
+let idade = parseInt(prompt("Qual a sua idade"))
 let carteira = prompt("Possui carteira de motorista")
 let carro = prompt("Possui carro?")
 
 
 if ( idade<18 || carteira == "não"){
-    console.log (nome + " você não pode dirigir!")   
+    console.log (nome + ", você não pode dirigir!")   
 
 }else if( carro == "não"){
-    console.log( nome + " compre um carro!")
+    console.log( nome + ", compre um carro!")
 
 
 }else{
-    console.log(nome + " você será o motorista")
+    console.log(nome + ", você será o motorista")
 
 
 }
